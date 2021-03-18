8f262c5574a7361c69511062e8887a256ad88077
lucarebuffi <lrebuffi@anl.gov>
2021-03-18 15:39:30 -0500

    Refactored Version

---
+++ orangecontrib/wavepy/widgets/tools/__init__.py
@@ -0,0 +1,22 @@
+__author__ = 'lrebuffi'
+
+"""
+=========
+Associate
+=========
+
+Widgets for association rules.
+
+"""
+
+# Category description for the widget registry
+
+NAME = "Tools"
+
+DESCRIPTION = "Widgets for WavePy - Tools"
+
+BACKGROUND = "#A9F5BC"
+
+ICON = "icons/utility.png"
+
+PRIORITY = 0.2
---
+++ orangecontrib/wavepy/widgets/wavepy/__init__.py
@@ -0,0 +1,22 @@
+__author__ = 'lrebuffi'
+
+"""
+=========
+Associate
+=========
+
+Widgets for association rules.
+
+"""
+
+# Category description for the widget registry
+
+NAME = "Wavepy"
+
+DESCRIPTION = "Widgets for WavePy"
+
+BACKGROUND = "#A9F5BC"
+
+ICON = "icons/wavepy.png"
+
+PRIORITY = 0.1
