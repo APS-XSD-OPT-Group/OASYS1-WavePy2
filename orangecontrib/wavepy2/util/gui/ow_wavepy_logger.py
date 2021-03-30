44bd4f337eaab088d7dbc770b80064ffe6b6d1b0
lucarebuffi <lrebuffi@anl.gov>
2021-03-29 20:06:22 -0500

    New features

--- orangecontrib/wavepy2/util/gui/ow_wavepy_logger.py
+++ orangecontrib/wavepy2/util/gui/ow_wavepy_logger.py
@@ -42,7 +42,6 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
 # POSSIBILITY OF SUCH DAMAGE.                                             #
 # #########################################################################
-from PyQt5.QtCore import QSettings
 from PyQt5.QtGui import QPalette, QColor, QFont
 
 from orangewidget import gui
